8385309548aa990ff1d8a8d877401f3cbf115ee8
Tim Swihart <chronoslynx@gmail.com>
2015-02-04 17:29:50 -0500

    new personal conf

--- personal/personal.el
+++ personal/personal.el
@@ -1,9 +1,3 @@
-;; Vim mode for editing
-;; (require 'evil-matchit)
-;;(global-evil-matchit-mode 1)
-;;(require 'evil-surround)
-;;(global-evil-surround-mode 1)
-
 ;; Save all temp files to /tmp/
 (setq backup-directory-alist
                 `((".*" . ,temporary-file-directory)))
@@ -18,7 +12,7 @@
 
 ;; Smartscan <https://github.com/mickeynp/smart-scan> setup
 (package-install 'smartscan)
-(smartscan-mode 1)
+(global-smartscan-mode 1)
 
 ;; Clang Format
 (setq clang-format-executable "clang-format-3.5")
@@ -31,5 +25,68 @@
 (setq eshell-where-to-jump 'begin)
 (setq eshell-review-quick-commands nil)
 (setq eshell-smart-space-goes-to-end t)
+
+;; C-x k to kill emacsclient session buffers
+(add-hook 'server-switch-hook
+          (lambda ()
+            (when (current-local-map)
+              (use-local-map (copy-keymap (current-local-map))))
+            (when server-buffer-clients
+              (local-set-key (kbd "C-x k") 'server-edit))))
+
+;; Aggressive indent mode. Mmmm...
+(require 'aggressive-indent)
+(global-aggressive-indent-mode 1)
+(add-to-list 'aggressive-indent-excluded-modes 'html-mode)
+(add-to-list 'aggressive-indent-excluded-modes 'inferior-sml-mode)
+
+;; C/C++/Objective-C via irony
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+;;fix issues with c++11 variables not being understood
+(require 'font-lock)
+(defun --copy-face (new-face face)
+  "Define NEW-FACE from existing FACE."
+  (copy-face face new-face)
+  (eval `(defvar ,new-face nil))
+  (set new-face new-face))
+(--copy-face 'font-lock-label-face  ; labels, case, public, private, proteced, namespace-tags
+         'font-lock-keyword-face)
+(--copy-face 'font-lock-doc-markup-face ; comment markups such as Javadoc-tags
+         'font-lock-doc-face)
+(--copy-face 'font-lock-doc-string-face ; comment markups
+         'font-lock-comment-face)
+(global-font-lock-mode t)
+(setq font-lock-maximum-decoration t)
+(add-hook 'c++-mode-hook
+          '(lambda()
+             (font-lock-add-keywords
+              nil '(;; complete some fundamental keywords
+                    ("\\<\\(void\\|unsigned\\|signed\\|char\\|short\\|bool\\|int\\|long\\|float\\|double\\)\\>" . font-lock-keyword-face)
+                    ;; add the new C++11 keywords
+                    ("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr\\|static_assert\\|thread_local\\|override\\|final\\)\\>" . font-lock-keyword-face)
+                    ("\\<\\(char[0-9]+_t\\)\\>" . font-lock-keyword-face)
+                    ;; PREPROCESSOR_CONSTANT
+                    ("\\<[A-Z]+[A-Z_]+\\>" . font-lock-constant-face)
+                    ;; hexadecimal numbers
+                    ("\\<0[xX][0-9A-Fa-f]+\\>" . font-lock-constant-face)
+                    ;; integer/float/scientific numbers
+                    ("\\<[\\-+]*[0-9]*\\.?[0-9]+\\([ulUL]+\\|[eE][\\-+]?[0-9]+\\)?\\>" . font-lock-constant-face)
+                    ;; user-types (customize!)
+                    ("\\<[A-Za-z_]+[A-Za-z_0-9]*_\\(t\\|type\\|ptr\\)\\>" . font-lock-type-face)
+                    ("\\<\\(xstring\\|xchar\\)\\>" . font-lock-type-face)
+                    ))
+             ) t)
 
 (provide 'personal)
